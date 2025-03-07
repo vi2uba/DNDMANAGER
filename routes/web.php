4b2b939e38a715bd8ac1ee7c27e43b495acc91e7
vi2uba <vinniuba2@gmail.com>
2025-03-07 22:23:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+// Search Route
+Route::get('/search', [SearchController::class, 'search'])->name('search');
