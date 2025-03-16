9364f767e4bd2e4a22d5546d3627b2bda8ace74a
vi2uba <vinniuba2@gmail.com>
2025-03-17 02:56:45 +0800

    Update Inventory.php

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    protected $table = 'inventories';
+    protected $table = 'inventory';
     protected $guarded = [];
     public $timestamps = false;
 
